a9fbe89ba0f822410bc3973ea194b52e73b74b88
Bintoutouray <bintoutouraym@gmail.com>
2018-12-21 11:50:21 +0000

    second commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function navigation() {
+    var na = document.querySelector('.nav');
+
+    if (na.style.display == "none") {
+        na.style.display = "block";
+    } 
+    else {
+        na.style.display = "none"
+    }
+}
